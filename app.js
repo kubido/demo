346e0ff9f86195eb7316cb7dd53b2d4c5863288f
Anthony Juan <anthonyjuan95@gmail.com>
2017-03-18 15:18:45 +0700

    edit line 12

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+// comment JUAN
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/project', project)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
